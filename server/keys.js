9eaf3ede9119ff3762dac4682a26ba500c9e2991
dimedical-corporativo <redessociales@dimedicalcorporativo.mx>
2023-11-03 14:12:22 -0600

    deploy to EB

--- server/keys.js
+++ server/keys.js
@@ -1,7 +1,7 @@
 export const keys = {
   redisHost: process.env.REDIS_HOST,
   redisPort: process.env.REDIS_PORT,
-  pgUser: process.env.PSUSER,
+  pgUser: process.env.PGUSER,
   pgHost: process.env.PGHOST,
   pgDatabase: process.env.PGDATABASE,
   pgPassword: process.env.PGPASSWORD,
